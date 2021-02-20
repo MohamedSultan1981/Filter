da854594868fc1d9cfc13a2c919ae899cad3728e
MohamedSultan1981 <50361347+MohamedSultan1981@users.noreply.github.com>
2021-02-20 20:18:39 +0200

    update

--- Filter/home/models.py
+++ Filter/home/models.py
@@ -5,11 +5,12 @@ class Factory(models.Model):
     
     Factory_Name=models.CharField(max_length=500)
     Factory_ID=models.CharField(max_length=20 ,primary_key=True)
-    Mobile_number=models.CharField(max_length=12,validators=[validators.MinValueValidator(11,message="please enter avalid mobile ")]
-    Factory_Address=models.AddressField(max_length=500)
+    
+    Factory_Address=models.TextField(max_length=500)
     Factory_city=models.CharField(max_length=50)
     Factory_gov=models.CharField(max_length=50)
     Factory_Manger=models.CharField(max_length=100)
     Factory_Activity=models.CharField(max_length=100)
+    Mobile_number=models.CharField(max_length=12)
     def __str__(self):
         return str(self.Factory_ID) 
--- Filter/home/urls.py
+++ Filter/home/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 from django.contrib.auth import views as auth_views
 from django_filters.views import FilterView
-from filter.home.filters import FactoryFilter
+from .filters import FactoryFilter
 urlpatterns = [
     path('', FilterView.as_view(filterset_class=FactoryFilter), name='search'),
 ]
