0327d89d75e050ae940d61577e46d26668a7612e
MohamedSultan1981 <50361347+MohamedSultan1981@users.noreply.github.com>
2021-02-20 19:56:47 +0200

    before testing using migrations

--- Filter/home/urls.py
+++ Filter/home/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 from . import views
 from django.contrib.auth import views as auth_views
-
+from django_filters.views import FilterView
+from filter.home.filters import FactoryFilter
 urlpatterns = [
-    path('', views.HomeView.as_view()),
+    path('', FilterView.as_view(filterset_class=FactoryFilter), name='search'),
 ]
