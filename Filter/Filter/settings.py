2ee611e40dcad6c7c83a3b61badf8c7030a5e663
MohamedSultan1981 <50361347+MohamedSultan1981@users.noreply.github.com>
2021-02-22 16:35:52 +0200

    serverconfig

--- Filter/Filter/settings.py
+++ Filter/Filter/settings.py
@@ -124,15 +124,16 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [
 
-    os.path.join(BASE_DIR, 'home/static'),
-]
+# Add these new lines
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
 
-STATIC_ROOT = os.path.join(
-    os.path.dirname(BASE_DIR), "static")
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
 LOGIN_REDIRECT_URL='/'
 PHONENUMBER_DB_FORMAT='INTERNATIONAL'
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
+#ALLOWED_HOSTS= ['localhost','server ip','127.0.0.1']
