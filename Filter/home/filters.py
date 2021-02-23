99c7318f363e143b775dc11a503f46f13813b56c
MohamedSultan1981 <50361347+MohamedSultan1981@users.noreply.github.com>
2021-02-23 07:34:02 +0200

    trying to use csv and remove lables

--- Filter/home/filters.py
+++ Filter/home/filters.py
@@ -1,14 +1,20 @@
 from .models import Factory
+from django import forms
 import django_filters
 from phonenumber_field.widgets import  PhoneNumberInternationalFallbackWidget
 class FactoryFilter(django_filters.FilterSet):
-  
+    Factory_ID: django_filters.CharFilter(label="hello")
 
-    
     class Meta:
         model = Factory
-        fields = ['Factory_Name', 'Factory_ID', 'Factory_Activity','Factory_gov','phone_number',]
+        fields = {'Factory_Name':['in']
+        , 'Factory_ID':['in']
+        , 'Factory_Activity':['in'],'Factory_gov':['in'],'phone_number':['in']}
+        
+      
         widgets = {
-            'phone_number': PhoneNumberInternationalFallbackWidget,
+            'phone_number': 'PhoneNumberInternationalFallbackWidget',
+            
         }
        
+
