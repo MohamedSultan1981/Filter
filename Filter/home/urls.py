df5e881744f8f6a5d2e3e30f4a48ff14343d7fbd
MohamedSultan1981 <50361347+MohamedSultan1981@users.noreply.github.com>
2021-02-25 15:22:47 +0200

    view

--- Filter/home/urls.py
+++ Filter/home/urls.py
@@ -4,5 +4,6 @@ from django.contrib.auth import views as auth_views
 from django_filters.views import FilterView
 from .filters import FactoryFilter,FACILITY_DATAFilter
 urlpatterns = [
-    path('', FilterView.as_view(filterset_class=FACILITY_DATAFilter), name='search'),
+    #path('', FilterView.as_view(filterset_class=FACILITY_DATAFilter), name='search'),
+    path('', views.FilteredFACILITY_DATAListView.as_view(), name='search'),
 ]
