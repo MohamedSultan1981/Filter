82fc345333cce1c667192137a8b63ccaed2ff740
MohamedSultan1981 <50361347+MohamedSultan1981@users.noreply.github.com>
2021-02-25 09:24:34 +0200

    oraclerun

--- Filter/home/filters.py
+++ Filter/home/filters.py
@@ -1,4 +1,4 @@
-from .models import Factory
+from .models import Factory,FACILITY_DATA
 import django_filters
 from phonenumber_field.widgets import  PhoneNumberInternationalFallbackWidget
 class FactoryFilter(django_filters.FilterSet):
@@ -11,4 +11,9 @@ class FactoryFilter(django_filters.FilterSet):
         widgets = {
             'phone_number': PhoneNumberInternationalFallbackWidget,
         }
-       
+class FACILITY_DATAFilter(django_filters.FilterSet):
+    NAME = django_filters.CharFilter(lookup_expr='icontains')
+
+    class Meta:
+        model = FACILITY_DATA
+        fields = ['NAME','CEO_NAME','DETAILED_ADDRESS','PRIMARY_MOBILE','Prouducts']
--- Filter/home/urls.py
+++ Filter/home/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 from django.contrib.auth import views as auth_views
 from django_filters.views import FilterView
-from .filters import FactoryFilter
+from .filters import FactoryFilter,FACILITY_DATAFilter
 urlpatterns = [
-    path('', FilterView.as_view(filterset_class=FactoryFilter), name='search'),
+    path('', FilterView.as_view(filterset_class=FACILITY_DATAFilter), name='search'),
 ]
