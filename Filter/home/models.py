91c51562f171b8b16264a5a5863cdec388e3e55c
MohamedSultan1981 <50361347+MohamedSultan1981@users.noreply.github.com>
2021-02-25 09:19:52 +0200

    copied models

--- Filter/home/models.py
+++ Filter/home/models.py
@@ -20,3 +20,43 @@ class Factory(models.Model):
     def __str__(self):
         return str(self.Factory_ID) 
 
+class INDUSTRIAL_PRODUCTS(models.Model):
+    
+    PRODUCT_ID = models.IntegerField(primary_key=True)
+    PRODUCT_TITLE = models.CharField(max_length=1000, null = True,blank=True,verbose_name="اسم المنتج")
+    
+    def __str__(self):
+        return str(self.PRODUCT_TITLE) 
+    class Meta:
+        
+        db_table = r'"IDA"."INDUSTRIAL_PRODUCTS"'  
+class FACILITY_DATA(models.Model):
+    
+    FACILITY_ID = models.CharField(max_length=1000,primary_key=True)
+    NAME = models.CharField(max_length=100, null = True,blank=True,verbose_name="اسم المنشأه")
+    CEO_NAME= models.CharField(max_length=200, null = True,blank=True,verbose_name="المدير")
+    DETAILED_ADDRESS=models.CharField(max_length=200, null = True,blank=True,verbose_name="العنوان")
+    PRIMARY_MOBILE=models.CharField(max_length=100, null = True,blank=True,verbose_name="الموبايل")
+    Prouducts = models.ManyToManyField(INDUSTRIAL_PRODUCTS, through='FACILITY_PRODUCTS',verbose_name="المنتجات")
+    def __str__(self):
+        return str(self.NAME) 
+    class Meta:
+
+        db_table = r'"IDA"."FACILITY_DATA"'
+
+ 
+
+
+class FACILITY_PRODUCTS(models.Model):
+    FACILITY= models.ForeignKey(FACILITY_DATA,on_delete=models.CASCADE)
+    PRODUCT= models.ForeignKey(INDUSTRIAL_PRODUCTS,on_delete=models.CASCADE)
+    UNIT_ID = models.IntegerField()
+    PRODUCT_QUANTITY = models.DecimalField( max_digits=38, decimal_places=3)
+
+    
+    def __str__(self):
+        return str(self.UNIT_ID) 
+       
+    class Meta:
+
+        db_table = r'"IDA"."FACILITY_PRODUCTS"'
