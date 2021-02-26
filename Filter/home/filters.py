df6ed84605a8b234941fa1b278044c8263b7fdb1
MohamedSultan1981 <50361347+MohamedSultan1981@users.noreply.github.com>
2021-02-26 07:16:38 +0200

    trying to use csv

--- Filter/home/filters.py
+++ Filter/home/filters.py
@@ -1,13 +1,14 @@
 from .models import Factory
 import django_filters
+from django_filters.widgets import CSVWidget
 from phonenumber_field.widgets import  PhoneNumberInternationalFallbackWidget
 class FactoryFilter(django_filters.FilterSet):
   
 
-    
+    my__reg=django_filters.CharFilter(lookup_expr='in',label="رقم السجل")
     class Meta:
         model = Factory
-        fields = ['Factory_Name', 'Factory_ID', 'Factory_Activity','Factory_gov','phone_number','my']
+        fields = ['Factory_Name', 'Factory_ID', 'Factory_Activity','Factory_gov','phone_number','my__reg']
         widgets = {
             'phone_number': PhoneNumberInternationalFallbackWidget,
         }
