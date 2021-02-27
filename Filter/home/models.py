266f3b93096a0d071259719dccf31dadf7212489
MohamedSultan1981 <50361347+MohamedSultan1981@users.noreply.github.com>
2021-02-27 14:16:22 +0200

    products running

--- Filter/home/models.py
+++ Filter/home/models.py
@@ -12,7 +12,7 @@ class INDUSTRIAL_PRODUCTS(models.Model):
     def __str__(self):
         return str(self.PRODUCT_TITLE) 
     class Meta:
-        ordering=['PRODUCT_ID']#could be wrong needs checking
+        ordering=['PRODUCT_TITLE']#could be wrong needs checking
         db_table = r'"IDA"."INDUSTRIAL_PRODUCTS"'  
 class FACILITY_DATA(models.Model):
     FACILITY_DATA_ID=models.CharField(max_length=50,unique=True)
@@ -25,10 +25,11 @@ class FACILITY_DATA(models.Model):
     def __str__(self):
         return str(self.NAME) 
     def Productquantities(self):
-        return FACILITY_PRODUCTS.objects.filter(FACILITY_ID=self.pk).order_by(PRODUCT_ID)
+        return FACILITY_PRODUCTS.objects.filter(FACILITY_ID=self.pk).order_by("PRODUCT_ID")
+        
         #return FACILITY_PRODUCTS.objects.filter(Factory_ID=self.pk, PRODUCT_ID__in= self.Prouducts.pk)
     class Meta:
-        ordering=('FACILITY_ID', )
+        #ordering=['FACILITY_ID']
 
         db_table = r'"IDA"."FACILITY_DATA"'
 
@@ -50,7 +51,7 @@ class MEASURING_UNITS (models.Model):
            db_table = r'"IDA"."MEASURING_UNITS"' 
 
 class FACILITY_PRODUCTS(models.Model):
-    FACILITY_ID= models.ForeignKey(FACILITY_DATA,to_field='FACILITY_ID',db_column='FACILITY_ID',on_delete=models.CASCADE)
+    FACILITY_ID= models.ForeignKey(FACILITY_DATA,to_field='FACILITY_ID',db_column='FACILITY_ID',on_delete=models.CASCADE,null=True,blank=True)
     PRODUCT_ID= models.ForeignKey(INDUSTRIAL_PRODUCTS,to_field='PRODUCT_ID',db_column='PRODUCT_ID',on_delete=models.CASCADE)
     UNIT =models.ForeignKey(MEASURING_UNITS  ,related_name='ty', on_delete=models.CASCADE)
     PRODUCT_QUANTITY = models.DecimalField( max_digits=38, decimal_places=3)
