70cd5098246c68493d06ef356f59f323d7cac843
MohamedSultan1981 <50361347+MohamedSultan1981@users.noreply.github.com>
2021-02-27 23:23:58 +0200

    filters

--- Filter/home/filters.py
+++ Filter/home/filters.py
@@ -5,11 +5,11 @@ from phonenumber_field.widgets import  PhoneNumberInternationalFallbackWidget
 class FactoryFilter(django_filters.FilterSet):
   
 
-    my__reg=django_filters.BaseInFilter(lookup_expr='in',label="رقم السجل",help_text="A list of county names, comma separated")
+    Factory_ID=django_filters.BaseInFilter(lookup_expr='in',label="رقم السجل",help_text="A list of county names, comma separated")
     class Meta:
         model = Factory
 
-        fields = ['Factory_Name']
+        fields = ['Factory_Name','Factory_ID']
         
        
         #fields = ['Factory_Name', 'Factory_ID', 'Factory_Activity','Factory_gov','phone_number','my__reg']
