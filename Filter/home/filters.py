10450d0735eb4cb5013bdf039afe0b420f05d8f9
MohamedSultan1981 <50361347+MohamedSultan1981@users.noreply.github.com>
2021-02-27 11:09:25 +0200

    need to modify reg

--- Filter/home/filters.py
+++ Filter/home/filters.py
@@ -5,7 +5,8 @@ from .models import FACILITY_DATA
 
 class FACILITY_DATAFilter(django_filters.FilterSet):
     NAME = django_filters.CharFilter(lookup_expr='icontains')
+    my__reg=django_filters.BaseInFilter(lookup_expr='in',label="رقم السجل",help_text="قم بادخال ارقام السجل مفصوله ب علامة الترقيم")
 
     class Meta:
         model = FACILITY_DATA
-        fields = ['NAME','CEO_NAME','DETAILED_ADDRESS','PRIMARY_MOBILE','Prouducts']
+        fields = ['NAME','CEO_NAME','DETAILED_ADDRESS','PRIMARY_MOBILE','Prouducts','my__reg"]
