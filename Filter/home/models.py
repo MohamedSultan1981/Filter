71bf9f30b9ac622074ddc6ac244d8631c1747b82
MohamedSultan1981 <50361347+MohamedSultan1981@users.noreply.github.com>
2021-02-27 13:09:18 +0200

    last bad

--- Filter/home/models.py
+++ Filter/home/models.py
@@ -15,7 +15,7 @@ class INDUSTRIAL_PRODUCTS(models.Model):
         ordering=('PRODUCT_TITLE', )#could be wrong needs checking
         db_table = r'"IDA"."INDUSTRIAL_PRODUCTS"'  
 class FACILITY_DATA(models.Model):
-    #FACILITY_DATA_ID=models.CharField(max_length=1000,primary_key=True)
+    FACILITY_DATA_ID=models.CharField(max_length=50,unique=True)
     FACILITY_ID = models.CharField(max_length=1000,primary_key=True)
     NAME = models.CharField(max_length=100, null = True,blank=True,verbose_name="اسم المنشأه")
     CEO_NAME= models.CharField(max_length=200, null = True,blank=True,verbose_name="المدير")
@@ -25,7 +25,7 @@ class FACILITY_DATA(models.Model):
     def __str__(self):
         return str(self.NAME) 
     def Productquantities(self):
-        return FACILITY_PRODUCTS.objects.filter(FACILITY_ID=self.pk).order_by("PRODUCT_ID")
+        return FACILITY_PRODUCTS.objects.filter(FACILITY=self.pk).order_by("PRODUCT_id")
         #return FACILITY_PRODUCTS.objects.filter(Factory_ID=self.pk, PRODUCT_ID__in= self.Prouducts.pk)
     class Meta:
 
@@ -34,7 +34,7 @@ class FACILITY_DATA(models.Model):
 class INDUSTRIAL_REGISTRY(models.Model):
         
     REGISTRY_ID = models.CharField(max_length=50,unique=True,primary_key=True)
-    FACILITY_DATA_ID=models.ForeignKey(FACILITY_DATA, to_field='FACILITY_ID',db_column='FACILITY_ID' ,related_name='my', on_delete=models.CASCADE)
+    FACILITY_DATA=models.ForeignKey(FACILITY_DATA, to_field='FACILITY_DATA_ID',db_column='FACILITY_DATA_ID' ,related_name='my', on_delete=models.CASCADE)
     REGISTRY_NUMBER=models.CharField(max_length=100)
     def __str__(self):
         return str(self.REGISTRY_NUMBER) 
@@ -44,14 +44,14 @@ class MEASURING_UNITS (models.Model):
     UNIT_ID = models.DecimalField( max_digits=38, decimal_places=0,primary_key=True)
     UNIT_TITLE_AR=   models.CharField(max_length=50)
     def __str__(self):
-        return str(self.unit_name)
+        return str(self.UNIT_TITLE_AR)
     class Meta:
            db_table = r'"IDA"."MEASURING_UNITS"' 
 
 class FACILITY_PRODUCTS(models.Model):
     FACILITY= models.ForeignKey(FACILITY_DATA,on_delete=models.CASCADE)
     PRODUCT= models.ForeignKey(INDUSTRIAL_PRODUCTS,on_delete=models.CASCADE)
-    UNIT_ID =models.ForeignKey(MEASURING_UNITS  ,related_name='ty', on_delete=models.CASCADE)
+    UNIT =models.ForeignKey(MEASURING_UNITS  ,related_name='ty', on_delete=models.CASCADE)
     PRODUCT_QUANTITY = models.DecimalField( max_digits=38, decimal_places=3)
        
     def __str__(self):
