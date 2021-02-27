d4aeabe49e2abb11ea7fd7c914554bbfcfce4b51
MohamedSultan1981 <50361347+MohamedSultan1981@users.noreply.github.com>
2021-02-27 11:34:22 +0200

    ww

--- Filter/home/views.py
+++ Filter/home/views.py
@@ -22,7 +22,7 @@ class FilteredFACILITY_DATAListView(SingleTableMixin, FilterView,ExportMixin):
         model =FACILITY_DATA
         filterset_class = FACILITY_DATAFilter
         #context_object_name = 'users'  # Default: object_list
-        paginate_by = 160
+        paginate_by = 200
         # def get_queryset(self):
         #     queryset = super().get_queryset()
         #     return FACILITY_DATAFilter(self.request.GET, queryset=queryset).qs 
