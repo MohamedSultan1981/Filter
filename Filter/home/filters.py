5031c5c95ee3171fe2c570654a08076b1925949d
MohamedSultan1981 <50361347+MohamedSultan1981@users.noreply.github.com>
2021-03-01 16:46:02 +0200

    working

--- Filter/home/filters.py
+++ Filter/home/filters.py
@@ -7,10 +7,8 @@ class FACILITY_DATAFilter(django_filters.FilterSet):
     NAME = django_filters.CharFilter(lookup_expr='icontains',label="اسم المنشأة")
     my__REGISTRY_NUMBER=django_filters.BaseInFilter(lookup_expr='in',label="رقم السجل",help_text="قم بادخال ارقام السجل مفصوله ب علامة الترقيم")
     CEO_NAME=django_filters.CharFilter(lookup_expr='icontains',label="اسم المدير")
-    Prouducts = django_filters.ModelMultipleChoiceFilter( 
-        queryset=INDUSTRIAL_PRODUCTS.objects.all() 
-    )
+   
     class Meta:
         model = FACILITY_DATA
-        fields = ['NAME','CEO_NAME','DETAILED_ADDRESS','PRIMARY_MOBILE','my__REGISTRY_NUMBER']
+        fields = ['NAME','CEO_NAME','DETAILED_ADDRESS','PRIMARY_MOBILE','my__REGISTRY_NUMBER','Prouducts']
       
