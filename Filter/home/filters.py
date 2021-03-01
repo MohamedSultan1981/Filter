432e04989bd6e1c6e11531f8f59a62e67ef8f790
MohamedSultan1981 <50361347+MohamedSultan1981@users.noreply.github.com>
2021-03-01 17:55:15 +0200

    final

--- Filter/home/filters.py
+++ Filter/home/filters.py
@@ -2,12 +2,13 @@ from .models import FACILITY_DATA
 import django_filters
 from phonenumber_field.widgets import  PhoneNumberInternationalFallbackWidget
 from .models import FACILITY_DATA,INDUSTRIAL_PRODUCTS
+from django import forms
 
 class FACILITY_DATAFilter(django_filters.FilterSet):
     NAME = django_filters.CharFilter(lookup_expr='icontains',label="اسم المنشأة")
     my__REGISTRY_NUMBER=django_filters.BaseInFilter(lookup_expr='in',label="رقم السجل",help_text="قم بادخال ارقام السجل مفصوله ب علامة الترقيم")
     CEO_NAME=django_filters.CharFilter(lookup_expr='icontains',label="اسم المدير")
-    DETAILED_ADDRESS=django_filters.CharFilter(lookup_expr='icontains',label=" العنوان")
+    DETAILED_ADDRESS=django_filters.CharFilter(lookup_expr='icontains',label=" العنوان",widget=forms.Textarea)
     class Meta:
         model = FACILITY_DATA
         fields = ['NAME','CEO_NAME','DETAILED_ADDRESS','PRIMARY_MOBILE','my__REGISTRY_NUMBER','Prouducts']
