6b9d40945954b11b67c8bb3fcfcc40b54b8cc3d8
ohilal <omniahilal@gmail.com>
2021-03-17 13:10:47 +0200

    table

--- Filter/home/filters.py
+++ Filter/home/filters.py
@@ -8,7 +8,7 @@ class FACILITY_DATAFilter(django_filters.FilterSet):
     NAME = django_filters.CharFilter(lookup_expr='icontains',label="اسم المنشأة")
     my__REGISTRY_NUMBER=django_filters.BaseInFilter(lookup_expr='in',label="رقم السجل",help_text="قم بادخال ارقام السجل مفصوله ب علامة الترقيم")
     CEO_NAME=django_filters.CharFilter(lookup_expr='icontains',label="اسم المدير")
-    DETAILED_ADDRESS=django_filters.CharFilter(lookup_expr='icontains',label=" العنوان",widget=forms.Textarea)
+    DETAILED_ADDRESS=django_filters.CharFilter(lookup_expr='icontains',label=" العنوان",widget=forms.Textarea(attrs={'rows':2, 'cols':4}))
     class Meta:
         model = FACILITY_DATA
         fields = ['NAME','CEO_NAME','DETAILED_ADDRESS','PRIMARY_MOBILE','my__REGISTRY_NUMBER','Prouducts']
